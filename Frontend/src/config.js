c8dbf86564dabdfc4789ef7bb09f22693fc85305
Ashay Panchal <69242688+Ashay1301@users.noreply.github.com>
2025-10-19 13:50:48 -0700

    production export

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -5,6 +5,6 @@ const developmentApiUrl = 'http://localhost:3000';
 const productionApiUrl = 'http://BeStill-Chatbot-env.eba-2iusiwup.us-west-1.elasticbeanstalk.com '; // <-- IMPORTANT: Replace with your actual backend URL
 
 // Export the correct URL based on the environment
-// export const API_URL = isProduction ? productionApiUrl : developmentApiUrl;
+export const API_URL = isProduction ? productionApiUrl : developmentApiUrl;
 
-export const API_URL = developmentApiUrl;
+// export const API_URL = developmentApiUrl;
