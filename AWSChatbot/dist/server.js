c119333f2df99981bf25122e7d40784bf73b6384
Ashay Panchal <69242688+Ashay1301@users.noreply.github.com>
2025-10-21 14:51:54 -0700

    commit

--- AWSChatbot/dist/server.js
+++ AWSChatbot/dist/server.js
@@ -14,7 +14,7 @@ const upload = multer({ storage: storage });
 const allowedOrigins = ['https://main.d1eow5fsdmo56z.amplifyapp.com', 'http://localhost:3001'];
 // === EXPRESS SETUP ===
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3001;
 app.use(cors({ origin: allowedOrigins }));
 // app.use(cors({origin: '*'}));
 app.use(express.json());
@@ -388,7 +388,7 @@ app.post("/api/clear", authenticateToken, async (req, res) => {
     resetDataEntry();
     res.status(200).json({ message: `History cleared for ${userId}.` });
 });
-app.listen(port, () => {
-    console.log(`✅ Server is running and ready for requests at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`✅ Server is running and ready for requests at http://localhost:${PORT}`);
 });
 //# sourceMappingURL=server.js.map
