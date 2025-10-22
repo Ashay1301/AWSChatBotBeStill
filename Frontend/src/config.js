0fdb4f9fadea2f8fa84a6d4a359b249547f1cd0f
Ashay Panchal <69242688+Ashay1301@users.noreply.github.com>
2025-10-22 14:01:35 -0700

    new elasticbeanstal

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,7 +2,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // Define your URLs
 const developmentApiUrl = 'http://localhost:3000';
-const productionApiUrl = 'https://Chatbot-env.eba-wzwnk7xj.us-west-2.elasticbeanstalk.com'; // <-- IMPORTANT: Replace with your actual backend URL
+const productionApiUrl = 'https://BeStill-App-env.eba-3ddwxm9b.us-west-2.elasticbeanstalk.com'; // <-- IMPORTANT: Replace with your actual backend URL
 
 // Export the correct URL based on the environment
 export const API_URL = isProduction ? productionApiUrl : developmentApiUrl;
